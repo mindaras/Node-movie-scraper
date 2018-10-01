7b423c4401ce0d765cc1f842b9080318e4593861
Mindaugas Lazauskas <mind.lazauskas@gmail.com>
2018-10-01 19:35:37 +0300

    added filtering

--- app.js
+++ app.js
@@ -12,49 +12,70 @@ app.get("/", (req, res) => {
 
 io.on("connection", socket => {
   parsePages(10);
+  socket.on("filter", data => {
+    socket.emit("reset");
+    parsePages(data);
+  });
 });
 
 http.listen(3000, () => console.log("listening on 3000"));
 
-const parseContent = url => {
+const parseContent = (url, { rating = 7, language, year }) => {
   request(url, (err, res, body) => {
     const $ = cheerio.load(body);
     const movies = $(".nwShCont");
     for (let i = 0; i < movies.length; i++) {
+      const data = {
+        rating: $(movies[i])
+          .find(".nwImdb a")
+          .text()
+          .replace("IMDb ", ""),
+        info: [],
+        img: $(movies[i])
+          .find("img")
+          .attr("data-src"),
+        url: `https://www.filmai.in/${$(movies[i])
+          .find(".nwSTinner a")
+          .attr("href")}`
+      };
+      $(movies[i])
+        .find(".nwPostInfoItem")
+        .each(
+          (i, e) =>
+            i === 0 ? (data.title = $(e).text()) : data.info.push($(e).text())
+        );
       if (
-        parseInt(
+        parseFloat(
           $(movies[i])
             .find(".nwImdb a")
             .text()
-            .replace("IMDb ", ""),
-          10
-        ) >= 7
+            .replace("IMDb ", "")
+        ) >= parseFloat(rating) &&
+        (language
+          ? data.info.some(inf => {
+              return inf.trim() === language.trim();
+            })
+          : true) &&
+        (year
+          ? data.info.some(inf => parseInt(inf, 10) >= parseInt(year, 10))
+          : true)
       ) {
-        const data = {
-          info: [],
-          img: $(movies[i])
-            .find("img")
-            .attr("data-src"),
-          url: `https://www.filmai.in/${$(movies[i])
-            .find(".nwSTinner a")
-            .attr("href")}`
-        };
-        $(movies[i])
-          .find(".nwPostInfoItem")
-          .each(
-            (i, e) =>
-              i === 0 ? (data.title = $(e).text()) : data.info.push($(e).text())
-          );
+        language ? data.info.some(inf => inf.trim() === language.trim()) : true;
         io.emit("loaded", data);
       }
     }
   });
 };
 
-const parsePages = count => {
+const parsePages = data => {
   let page = 1;
+  let count = 1;
+  typeof data === "number"
+    ? (count = data)
+    : (count = parseInt(data.pageCount, 10));
   while (page <= count) {
-    parseContent(url.replace("%page%", page));
+    delete data.pageCount;
+    parseContent(url.replace("%page%", page), data || {});
     page++;
   }
 };
